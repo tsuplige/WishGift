4454f8478ebf967da6f79954a18ffd2c37ef17b8
Tsuplige <thomassupligeon@gmail.com>
2024-04-15 18:22:30 +0200

    create wishlist and authnetication models

--- WishGift/WishGift/settings.py
+++ WishGift/WishGift/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authnetication",
+    "wishlist"
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "authentication.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
