ef5a7c88a7f7c2a3a491c9c20fd3cebd7392d72b
Tsuplige <thomassupligeon@gmail.com>
2024-04-29 01:00:30 +0200

    add login

--- WishGift/authentication/views.py
+++ WishGift/authentication/views.py
@@ -5,21 +5,24 @@ from django.shortcuts import redirect, render
 
 
 def login_page(request):
-    form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiants invalides.'
-    return render(request, 'authentication/login.html', context={
-        'form': form, 'message': message})
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = forms.LoginForm()
+        message = ''
+        if request.method == 'POST':
+            form = forms.LoginForm(request.POST)
+            if form.is_valid():
+                user = authenticate(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password'],
+                )
+                if user is not None:
+                    login(request, user)
+                    return redirect('home')
+            message = 'Identifiants invalides.'
+        return render(request, 'authentication/login.html', context={
+            'form': form, 'message': message})
 
 
 def signup_page(request):
