d6d96e54dff0b171a71dab43aa402dbc4fd95a31
Tsuplige <thomassupligeon@gmail.com>
2024-05-02 11:01:06 +0200

    add delete methode

--- WishGift/wishlist/views.py
+++ WishGift/wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist, Item
@@ -21,3 +21,16 @@ def friendlist(request):
 @login_required
 def friendwishlist(request):
     return render(request, 'wishlist/friendwishlist.html')
+
+
+@login_required
+def item_delete(request, id):
+    item = Item.objects.get(id=id)
+
+    if request.method == 'POST':
+        if request.user == item.user or request.user.is_superuser:
+            item.delete()
+            return redirect('home')
+        return redirect('home')
+
+    return render(request, 'app/item_delete.html', {'item': item})
