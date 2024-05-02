7c02ed4affe38070a996ff5820e89f514825ac5a
Tsuplige <thomassupligeon@gmail.com>
2024-05-02 11:00:37 +0200

    correct redirec signup

--- WishGift/authentication/views.py
+++ WishGift/authentication/views.py
@@ -31,9 +31,8 @@ def signup_page(request):
         form = forms.SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # auto-login user
             login(request, user)
-            return redirect(settings.LOGIN_REDIRECT_URL)
+            return redirect("home")
     return render(request, 'authentication/signup.html',
                   context={'form': form})
 
